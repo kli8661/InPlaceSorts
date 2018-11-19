001aadf6905e72111db8fa5372397d883d554e65
BT_1N3_17 <kli8661@bths.edu>
2018-11-19 09:40:33 -0500

    Done

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -42,13 +42,25 @@ public class InPlaceSorts {
     public static void bubbleSort(String[] list1)
     {
         String temp;
-        for (int i = 0; i < list1.length - 1; i++)
-        {
-            if (list1[i].compareTo(list1[i + 1]) > 0)
+        int swaps = 0;
+        boolean swapped = true;
+        while(swapped) {
+            for (int i = 0; i < list1.length - 1; i++) {
+                if (list1[i].compareTo(list1[i + 1]) > 0) {
+                    temp = list1[i];
+                    list1[i] = list1[i + 1];
+                    list1[i + 1] = temp;
+                    swaps++;
+                }
+            }
+            if(swaps > 0)
             {
-                temp = list1[i];
-                list1[i] = list1[i + 1];
-                list1[i + 1] = temp;
+                swaps = 0;
+            }
+            else
+            {
+                if(swaps == 0)
+                    swapped = false;
             }
         }
     }
--- src/Runner.java
+++ src/Runner.java
@@ -3,13 +3,16 @@ public class Runner {
     public static void main(String[] args)
     {
         InPlaceSorts list1 = new InPlaceSorts();
-        String[] arrayStr = list1.generateStrings(6);
+        String[] arrayStr = list1.generateStrings(10);
+        System.out.println("Bubble Sorting Strings");
         list1.bubbleSort(arrayStr);
         System.out.println(Arrays.toString(arrayStr));
-        int[] arrayInt = list1.generateInts(6);
+        int[] arrayInt = list1.generateInts(10);
+        System.out.println("Insertion Sorting Ints");
         list1.insertionSort(arrayInt);
         System.out.println(Arrays.toString(arrayInt));
-        double[] arrayDouble = list1.generateDoubles(6);
+        double[] arrayDouble = list1.generateDoubles(10);
+        System.out.println("Selection Sorting Doubles");
         list1.selectionSort(arrayDouble);
         System.out.println(Arrays.toString(arrayDouble));
     }
